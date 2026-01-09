e26b4fd751abb780cb5081abbe6b39fd0c2462d1
asp <asp@Aptechnk1.edu>
2026-01-09 20:54:07 +0500

    practical test

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.post("/registration", async(req, res)=>{
     const {username,useremail,userpass} = req.body;
     const salt = await bcrypt.genSalt(10);
     var hashedPass = await bcrypt.hash(userpass, salt)
+
+    if(!username || !useremail || !userpass){
+        return res.send("All field need to Fill!");
+    }
     var user = {
         username,
         useremail,
@@ -26,17 +30,29 @@ app.post("/registration", async(req, res)=>{
 
 
 
-app.post("/login", async(req, res)=>{
+app.post('/Login',async(req,res)=>{
+    const {useremail,userpass} = req.body;
 
-    const {useremail, userpass} = req.body;
     if(!useremail || !userpass){
-        return res.send("please fill all the details")
+        return res.send("Kindly fill all the fields")
     }
-    var userAvail = await UserdataSchema.findById("695bdaff7af1f6c9d1c2af66")
 
-    return res.send(userAvail)
+    var userAvail = await UserdataSchema.findOne({useremail});
+    if(!userAvail){
+        return res.send("User not found, Kindly register first");
+    }
+
+    //var DBPass = userAvail.userpass;
+
+    var ValidOrNot = await bcrypt.compare(userpass, userAvail.userpass);
+
+    if(!ValidOrNot){
+        return res.send("something went wrong");
+    }
+
+    return res.send("Login Successful");
+
 })
-
 
 
 
